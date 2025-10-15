8e311f48e33a831bdb3b739ee8147b9569179333
dazdaz <darren@dazdaz.org>
2025-10-15 13:16:59 +0200

    Moved contents of https://github.com/dazdaz/cloudbuild-optimizer into cloudbuild-optimizer/

---
+++ cloudbuild-optimizer/README.md
@@ -0,0 +1,49 @@
+# Cloud Build Optimizer
+
+This tool allows you to quickly run your Cloud Build configuration against multiple machine types in parallel. 
+
+Once the jobs are complete you get a read out of the time each took and how much they cost.
+
+![Example screen recording](example/demo.gif)
+
+## Quick Start
+
+This repo includes and example `cloudbuild.yaml` that compiles the Linux kernel. 
+Below are the steps to test out that configuration across the available Cloud Build machine types.
+
+1. Clone this repo and change directories to the `example` folder:
+
+    ```shell
+    git clone https://github.com/viglesiasce/cloudbuild-optimizer
+    cd cloudbuild-optimizer/example
+    ```
+
+1. Download the latest release:
+
+    ```shell
+    curl -L https://github.com/viglesiasce/cloudbuild-optimizer/releases/download/v0.2.0/cloudbuild-optimizer_0.2.0_$(uname)_$(uname -m) -o cloudbuild-optimizer
+    chmod +x cloudbuild-optimizer
+    ```
+
+1. Run the tool.
+
+    ```shell
+    $ ./cloudbuild-optmizer
+    ```
+
+    You should see output as follows:
+    
+    ```shell
+    View your builds here: https://console.cloud.google.com/cloud-build/builds
+
+    Starting build on DEFAULT...
+    Starting build on E2_HIGHCPU_32...
+    Starting build on E2_HIGHCPU_8...
+    Build completed on E2_HIGHCPU_32 in 5.64 minutes.
+    Build completed on E2_HIGHCPU_8 in 8.56 minutes.
+    Build completed on DEFAULT in 41.50 minutes.
+
+    Build took 5m38.502519808s minutes on E2_HIGHCPU_32 and cost $0.361
+    Build took 8m33.552805059s minutes on E2_HIGHCPU_8 and cost $0.137
+    Build took 41m29.819853143s minutes on DEFAULT and cost $0.124
+    ```
---
+++ cloudbuild-optimizer/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+type Result struct {
+	Err         error
+	Duration    time.Duration
+	MachineType string
+}
+
+func main() {
+	// Mapping of machine types to their price per minute
+	machineTypes := map[string]float64{
+		"DEFAULT":       0.003,
+		"E2_HIGHCPU_8":  0.016,
+		"E2_HIGHCPU_32": 0.064,
+	}
+
+	// Set up a channel to receive results from each build
+	resultChan := make(chan Result)
+	done := make(chan bool)
+
+	fmt.Printf("View your builds here: https://console.cloud.google.com/cloud-build/builds\n\n")
+
+	// Run cloud build for each machine type
+	for mt := range machineTypes {
+		go runBuild(mt, resultChan, done)
+	}
+
+	results := []Result{}
+	for i := 0; i < len(machineTypes); i++ {
+		result := <-resultChan
+		results = append(results, result)
+	}
+	close(resultChan)
+
+	// Print table of costs
+	fmt.Println()
+	for _, result := range results {
+		fmt.Printf("Build took %v minutes on %v and cost $%.3f\n", result.Duration, result.MachineType, result.Duration.Minutes()*machineTypes[result.MachineType])
+	}
+}
+
+func runBuild(machineType string, result chan Result, done chan bool) {
+	command := "gcloud"
+
+	args := strings.Split("builds submit .", " ")
+	if machineType != "DEFAULT" {
+		args = append(args, fmt.Sprintf("--machine-type=%s", machineType))
+	}
+	cmd := exec.Command(command, args...)
+	// Start build timer
+	startTime := time.Now()
+
+	// Run command
+	fmt.Printf("Starting build on %s...\n", machineType)
+	cmd.Env = []string{"PYTHONUNBUFFERED=TRUE"}
+	cmd.Start()
+
+	err := cmd.Wait()
+	endTime := time.Now()
+	diff := endTime.Sub(startTime)
+	fmt.Printf("Build completed on %s in %.2f minutes.\n", machineType, diff.Minutes())
+
+	// Send result of build
+	result <- Result{Err: err, Duration: diff, MachineType: machineType}
+}
